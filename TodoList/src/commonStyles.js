537e5514261bc0a3beec542e8e46de021729122e
unknown <gugaraujo01@gmail.com>
2022-11-04 10:33:36 -0400

    initial commit

---
+++ TodoList/src/commonStyles.js
@@ -0,0 +1,9 @@
+export default {
+    fontFamily: 'Lato',
+    colors: {
+        secundary: '#FFF',
+        mainText: '#222',
+        subText: '#555',
+    }
+
+}
---
+++ TodoList/src/screens/TaskList.js
@@ -0,0 +1,74 @@
+import React, { Component} from 'react'
+import { Text, View, ImageBackground, StyleSheet, FlatList } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import commonStyles from '../commonStyles'
+import Task from '../components/Task'
+
+export default class TaskList extends Component {
+
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: 'Comprar livro de React',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        }, 
+        {
+            id: Math.random(),
+            desc: 'Comprar livro de Js',
+            estimateAt: new Date(),
+            doneAt: null,
+        },]
+    }
+
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <FlatList data={this.state.tasks} keyExtractor={item => `${item.id}`}
+                     renderItem={({item}) => <Task {...item}/>} />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title:{
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secundary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secundary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+    }
+})
